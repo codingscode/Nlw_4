f29d744011ecc7fd884a4a20d0d0aa5c33d9df63
codingscode <heronnestg@hotmail.com>
2021-02-25 11:22:37 -0300

    trilha node parte 6

--- trilha_node/src/server.ts
+++ trilha_node/src/server.ts
@@ -6,9 +6,15 @@ const app = express()
 get -> acessar, post -> salvar/enviar, put -> alterar/colocar, delete -> apagar, patch -> alteração específica
 */
 
-app.get('/usuarios', (req, resp) => {
+app.get('/', (req, resp) => {
     return resp.json({mensagem: 'Olá mundo - NLW4', dia: 1})
 })
+
+app.post('/', (req, resp) => {
+    // recebeu os dados para salvar
+    return resp.json({ mensagem: 'Os dados foram salvos com sucesso!' })
+})
+ 
 
 app.listen(3333, () => console.log('Servidor está rodando!'))
 
