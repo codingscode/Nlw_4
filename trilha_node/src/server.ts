cc8339a7e7b926e27ca41dcdb421987cd20b0e1d
codingscode <heronnestg@hotmail.com>
2021-02-25 10:52:41 -0300

    trilha node parte 4

--- trilha_node/src/server.ts
+++ trilha_node/src/server.ts
@@ -2,6 +2,14 @@ import express from 'express'
 
 const app = express()
 
+/* 
+get -> acessar, post -> salvar/enviar, put -> alterar/colocar, delete -> apagar, patch -> alteração específica
+*/
+
+app.get('/usuarios', (req, resp) => {
+    return resp.send('Olá Mundo - NLW4')
+})
+
 app.listen(3333, () => console.log('Servidor está rodando!'))
 
 
