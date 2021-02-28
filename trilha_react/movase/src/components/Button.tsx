471a34824d1ce2e27ed4cbc5175122d392bed412
codingscode <heronnestg@hotmail.com>
2021-02-28 20:49:15 -0300

    trilha react parte 6

--- trilha_react/movase/src/components/Button.tsx
+++ trilha_react/movase/src/components/Button.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 interface ButtonProps {
     color: string,
@@ -7,10 +7,15 @@ interface ButtonProps {
 
 
 export function Button(props: ButtonProps) {
+     const [counter, setCounter] = useState(1)
+
+     function increment() {
+         setCounter(p => p + 1)
+     }
 
      return(
-         <button type="button" style={{background: props.color, color: 'white'}} >
-             {props.children}
+         <button type="button" style={{background: props.color, color: 'white'}} onClick={increment} >
+             {props.children} || <strong>{counter}</strong>
          </button>
      )
 }
