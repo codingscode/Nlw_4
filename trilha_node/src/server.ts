2b1edba54ec9f0bdc4fd6bd94b89389ab9ffe761
codingscode <heronnestg@hotmail.com>
2021-02-28 20:10:20 -0300

    trilha node parte 13

--- trilha_node/src/server.ts
+++ trilha_node/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import 'reflect-metadata'
 import './database'  // './database/index.ts'
+import { router } from './routes'
 
 const app = express()
 
@@ -8,14 +9,17 @@ const app = express()
 get -> acessar, post -> salvar/enviar, put -> alterar/colocar, delete -> apagar, patch -> alteração específica
 */
 
-app.get('/', (req, resp) => {
+app.use(router)
+
+
+/* app.get('/', (req, resp) => {
     return resp.json({mensagem: 'Olá mundo - NLW4', dia: 1})
 })
 
 app.post('/', (req, resp) => {
     // recebeu os dados para salvar
     return resp.json({ mensagem: 'Os dados foram salvos com sucesso!' })
-})
+}) */
  
 
 app.listen(3333, () => console.log('Servidor está rodando!'))
