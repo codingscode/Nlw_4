4756351c906494bf0d6dddce211695def7a5f6da
codingscode <heronnestg@hotmail.com>
2021-03-05 20:32:19 -0300

    trilha node parte 26

--- trilha_node/src/__tests__/First.test.ts
+++ trilha_node/src/__tests__/First.test.ts
@@ -2,5 +2,8 @@ describe('First', () => {
     it('deve ser possivel somar 2 números', () => {
        expect(2 + 2).toBe(4)
     })
+    it('deve ser possivel somar 2 números', () => {
+       expect(2 + 3).toBe(10)
+    })
 })
 
