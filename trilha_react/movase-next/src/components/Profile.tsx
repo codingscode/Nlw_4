2f31a6c2613f2fb267761c0a7abb1b829ddccea1
codingscode <heronnestg@hotmail.com>
2021-03-06 20:33:42 -0300

    trilha react parte 24

--- trilha_react/movase-next/src/components/Profile.tsx
+++ trilha_react/movase-next/src/components/Profile.tsx
@@ -7,7 +7,10 @@ export function Profile() {
          <img src="https://github.com/codingscode.png" alt="Heronnes Pereira" />
          <div>
            <strong>Heronnes Pereira</strong>
-           <p>Level 1</p>
+           <p>
+               <img src="icons/level.svg" alt="Level" />
+               Level 1
+           </p>
          </div>
        </div>
     )
