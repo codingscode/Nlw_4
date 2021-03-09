852750fa26a6827a2906ca47a251beb5b3e7a3ff
codingscode <heronnestg@hotmail.com>
2021-03-09 19:51:11 -0300

    trilha react parte 30

--- trilha_react/movase-next/src/components/CountDown.tsx
+++ trilha_react/movase-next/src/components/CountDown.tsx
@@ -4,16 +4,19 @@ import styles from '../styles/components/CountDown.module.css'
 export default function CountDown() {
 
     return (
-        <div className={styles.countdownContainer} >
-            <div>
-                <span>2</span>
-                <span>5</span>
+        <div>
+            <div className={styles.countdownContainer} >
+                <div>
+                    <span>2</span>
+                    <span>5</span>
+                </div>
+                <span>:</span>
+                <div>
+                    <span>0</span>
+                    <span>0</span>
+                </div>
             </div>
-            <span>:</span>
-            <div>
-                <span>0</span>
-                <span>0</span>
-            </div>
+            <button type="button" className={styles.countdownButton} >Iniciar ciclo</button>
         </div>
     )
 }
