ec89ac80be215afed0b7c1402297c85c3dae6469
codingscode <heronnestg@hotmail.com>
2021-03-10 21:32:35 -0300

    trilha react parte 42

--- trilha_react/movase-next/src/pages/index.tsx
+++ trilha_react/movase-next/src/pages/index.tsx
@@ -5,6 +5,7 @@ import { Profile } from '../components/Profile'
 import styles from '../styles/pages/Home.module.css'
 import CompletedChallenges from '../components/CompletedChallenges'
 import CountDown from '../components/CountDown'
+import { ChallengeBox } from '../components/ChallengeBox'
 
 
 export default function Home() {
@@ -21,7 +22,9 @@ export default function Home() {
               <CompletedChallenges />
               <CountDown />
             </div>
-            <div></div>
+            <div>
+               <ChallengeBox />
+            </div>
          </section>
       </div>
     )
