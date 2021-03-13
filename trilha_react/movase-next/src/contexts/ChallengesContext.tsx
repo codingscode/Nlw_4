60f0709ea8528b78576984af7b437c2c2ca1548f
codingscode <heronnestg@hotmail.com>
2021-03-12 21:24:29 -0300

    trilha react parte 48

--- trilha_react/movase-next/src/contexts/ChallengesContext.tsx
+++ trilha_react/movase-next/src/contexts/ChallengesContext.tsx
@@ -1,17 +1,29 @@
-import { createContext, useState } from 'react'
+import { createContext, ReactNode, useState } from 'react'
 
 
 export const ChallengesContext = createContext({})
 
-export function ChallengesProvider({ children }) {
+interface ChallengesProviderProps {
+    children: ReactNode
+}
+ 
+
+export function ChallengesProvider({ children }: ChallengesProviderProps) {
     const [level, setLevel] = useState(1)
-    
+    const [currentExperience, setCurrentExperience] = useState(0)
+    const [challengesCompleted, setChallengesCompleted] = useState(0)
+
+
     function levelUp() {
         setLevel(level + 1)
     }
 
+    function startNewChallenge() {
+        console.log('New challenge')
+    }
+
     return (
-        <ChallengesContext.Provider value={{ level, levelUp }} >
+        <ChallengesContext.Provider value={{ level, currentExperience, challengesCompleted, levelUp, startNewChallenge }} >
             {children}
         </ChallengesContext.Provider>
     )
