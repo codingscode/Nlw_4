89d46f7e0f41363e00adb4575fdcdfec91b20146
codingscode <heronnestg@hotmail.com>
2021-03-12 21:12:14 -0300

    trilha react parte 47

--- trilha_react/movase-next/src/contexts/ChallengesContext.tsx
+++ trilha_react/movase-next/src/contexts/ChallengesContext.tsx
@@ -1,6 +1,19 @@
-import { createContext } from 'react'
-
+import { createContext, useState } from 'react'
 
 
 export const ChallengesContext = createContext({})
+
+export function ChallengesProvider({ children }) {
+    const [level, setLevel] = useState(1)
+    
+    function levelUp() {
+        setLevel(level + 1)
+    }
+
+    return (
+        <ChallengesContext.Provider value={{ level, levelUp }} >
+            {children}
+        </ChallengesContext.Provider>
+    )
+}
 
