a76547d131d5a0df9c1f4dd4720ab9d8f4045371
codingscode <heronnestg@hotmail.com>
2021-03-15 21:18:32 -0300

    trilha react parte 64

--- trilha_react/movase-next/src/pages/index.tsx
+++ trilha_react/movase-next/src/pages/index.tsx
@@ -8,31 +8,39 @@ import CompletedChallenges from '../components/CompletedChallenges'
 import CountDown from '../components/CountDown'
 import { ChallengeBox } from '../components/ChallengeBox'
 import { CountdownProvider } from '../contexts/CountdownContext'
+import { ChallengesProvider } from '../contexts/ChallengesContext'
 
+interface HomeProps {
+   level: number
+   currentExperience: number
+   challengesCompleted: number
+}
 
-export default function Home(props) {
+export default function Home(props: HomeProps) {
     console.log(props)
 
     return (
-      <div className={styles.container} >
-         <Head>
-            <title>Início | mova-se</title>
-         </Head>
-         <ExperienceBar />
+      <ChallengesProvider level={props.level} currentExperience={props.currentExperience} challengesCompleted={props.challengesCompleted} >
+         <div className={styles.container} >
+            <Head>
+               <title>Início | mova-se</title>
+            </Head>
+            <ExperienceBar />
 
-         <CountdownProvider>
-            <section>
-               <div>
-                  <Profile />
-                  <CompletedChallenges />
-                  <CountDown />
-               </div>
-               <div>
-                  <ChallengeBox />
-               </div>
-            </section>
-         </CountdownProvider>
-      </div>
+            <CountdownProvider>
+               <section>
+                  <div>
+                     <Profile />
+                     <CompletedChallenges />
+                     <CountDown />
+                  </div>
+                  <div>
+                     <ChallengeBox />
+                  </div>
+               </section>
+            </CountdownProvider>
+         </div>
+      </ChallengesProvider>
     )
 }
 
@@ -41,7 +49,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
    const { level, currentExperience, challengesCompleted } = ctx.req.cookies
       
    return {
-      props: { level, currentExperience, challengesCompleted }
+      props: { level: Number(level), currentExperience: Number(currentExperience), challengesCompleted: Number(challengesCompleted) }
    }
 }
 
