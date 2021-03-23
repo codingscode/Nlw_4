265eb29f1147f49aff033529607f500fc1cce8e5
codingscode <heronnestg@hotmail.com>
2021-03-22 22:32:08 -0300

    trilha node parte 45 hj so node

--- trilha_node/src/controllers/SendMailController.ts
+++ trilha_node/src/controllers/SendMailController.ts
@@ -31,17 +31,19 @@ class SendMailController {
            })
         }
 
-        const variables = { name: user.name, title: survey.title, description: survey.description, 
-                            user_id: user.id, link: process.env.URL_MAIL }
-                          
+        
         const npsPath = resolve(__dirname, '..', 'views', 'emails', 'npsMail.hbs')
-
+        
         const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
            where: {user_id: user.id, value: null},
            relations: ['user', 'survey']
-        })
+         })
+       
+        const variables = { name: user.name, title: survey.title, description: survey.description, 
+                             id: '', link: process.env.URL_MAIL }
       
         if (surveyUserAlreadyExists) {
+            variables.id = surveyUserAlreadyExists.id
             await SendMailService.execute(email, survey.title, variables, npsPath)
             return response.json(surveyUserAlreadyExists)
         }
@@ -54,7 +56,7 @@ class SendMailController {
         await surveysUsersRepository.save(surveyUser)
         // enviar e-mail para o usuário
 
-
+        variables.id = surveyUser.id
         await SendMailService.execute(email, survey.title, variables, npsPath)
         
         return response.json(surveyUser)
